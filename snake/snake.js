d326a83ff1bc12de090b7c6058a8f3be4bea822e
Lucas Gama <lucasgdon@gmail.com>
2023-04-07 15:00:24 -0300

    Atualização de visuais, inclusão de score, high score, aviso de high score, aviso de game over, botão de reiniciar ou reiniciar apertando enter, página inicial com links para os jogos

--- snake/snake.js
+++ snake/snake.js
@@ -1,24 +1,28 @@
 // CANVAS
-var blockSize = 25;
-var rows = 20;
-var cols = 20;
-var board;
-var context;
+let blockSize = 15;
+let rows = 30;
+let cols = 30;
+let board;
+let context;
 
 // CABEÇA
-var snakeHeadX = blockSize * 5;
-var snakeHeadY = blockSize * 5;
+let snakeHeadX = blockSize * 5;
+let snakeHeadY = blockSize * 5;
 
-var velocityX = 0;
-var velocityY = 0;
+let velocityX = 0;
+let velocityY = 0;
 
-var snakeBody = [];
+let snakeBody = [];
 
 // COMIDA
-var foodX;
-var foodY;
+let foodX;
+let foodY;
 
-var gameOver = false;
+// BORDAS
+let borderWidth = 2.5;   
+let offset = borderWidth * 2;
+
+let gameOver = false;
 
 window.onload = function(){
     board = document.getElementById("board");
@@ -29,22 +33,95 @@ window.onload = function(){
     placeFood();
     document.addEventListener("keyup", changeDirection);
     setInterval(update, 1000/10); // 100 milisegundos
+
+    let high_score = localStorage.getItem("snake_high_score");
+ 
+    if (high_score === null) {
+        localStorage.setItem("snake_high_score", 0);
+        document.getElementById("high_score").innerHTML = 0;
+    }else{
+        document.getElementById("high_score").innerHTML = parseInt(high_score);
+    }
+
 }
 
 function update(){
 
     if(gameOver){
+
+        let current_score = parseInt(localStorage.getItem("snake_score"));
+        let high_score = parseInt(localStorage.getItem("snake_high_score"));
+        let new_high_score = document.getElementById("new_high_score");
+        let restart_btn = document.getElementById("restart");
+        let enter_tip = document.getElementById("enter_tip");
+
+        if(current_score > high_score){
+            localStorage.setItem("snake_high_score", current_score);
+            document.getElementById("high_score").innerHTML = current_score;
+
+            new_high_score.style.display = 'block';
+        }
+        
+        restart_btn.style.display = 'block';
+        enter_tip.style.display = 'block';
+        
+        localStorage.setItem("snake_score", 0);
+
+        // ===============
+
+        board = document.getElementById("board");
+        board.height = rows * blockSize;
+        board.width = cols * blockSize;
+        context = board.getContext("2d");   
+        context.clearRect(0, 0, board.width, board.height);
+
+        context.font = "bold  30px Courier New";
+        context.textAlign = "center";
+        context.fillText("GAME OVER", board.width/2, board.height/2);
+
+        // ==============
+
+
+        // Execute a function when the user presses a key on the keyboard
+        document.addEventListener("keypress", function(event) {
+            // If the user presses the "Enter" key on the keyboard
+            if (event.key === "Enter") {
+            // Cancel the default action, if needed
+            event.preventDefault();
+            location.reload()
+            }
+        });
+
+
         return;
     }
 
-    context.fillStyle="black";
+    // CANVAS
+    context.fillStyle="#778a7a";
     context.fillRect(0, 0, board.width, board.height);
 
-    context.fillStyle="yellow";
-    context.fillStyle="b";
+    // COMIDA
+
+    context.fillStyle = "#627769";
+    context.fillRect(foodX - borderWidth, foodY - borderWidth, blockSize + offset, blockSize + offset);
+    context.fillStyle = "#4c5e52";
     context.fillRect(foodX, foodY, blockSize, blockSize);
 
     if(snakeHeadX == foodX && snakeHeadY == foodY){
+
+        let current_score = parseInt(localStorage.getItem("snake_score"));
+
+        if (current_score === null) {
+            localStorage.setItem("snake_score", parsetInt(1));
+        }else{
+            localStorage.setItem("snake_score", parseInt(current_score + 1));
+        }
+
+        let upd_current_score = parseInt(localStorage.getItem("snake_score"));
+
+
+        document.getElementById("score").innerHTML = upd_current_score;
+
         snakeBody.push([foodX, foodY]);
         placeFood();
     }
@@ -56,31 +133,34 @@ function update(){
         snakeBody[0] = [snakeHeadX, snakeHeadY];
     }
 
-    context.fillStyle = "lime";
+    context.fillStyle = "#627769";
+    
     snakeHeadX += velocityX * blockSize;
     snakeHeadY += velocityY * blockSize;
+    context.fillRect(snakeHeadX - borderWidth, snakeHeadY - borderWidth, blockSize + offset, blockSize + offset);
+    context.fillStyle = "#27302a";
+
     context.fillRect(snakeHeadX, snakeHeadY, blockSize, blockSize);
 
-    for(let i = 0; i < snakeBody.length; i++){
-        context.fillRect(snakeBody[i][0], snakeBody[i][1], blockSize, blockSize);
+    for(const element of snakeBody){
+        context.fillStyle = "#627769";
+        context.fillRect(element[0] - borderWidth, element[1] - borderWidth, blockSize + offset, blockSize + offset);
+        context.fillStyle = "#27302a";
+        context.fillRect(element[0], element[1], blockSize, blockSize);
     }
 
     // CONDIÇÕES DE GAME OVER
 
     // PAREDES
-    if(snakeHeadX < 0 || snakeHeadX > cols*blockSize || snakeHeadY < 0 || snakeHeadY > rows*blockSize ){
+    if(snakeHeadX < 0 || snakeHeadX >= cols*blockSize || snakeHeadY < 0 || snakeHeadY >= rows*blockSize ){
         gameOver = true;
-        alert("Game over!");
-        location.reload()
     }
 
     // BATER EM SI MESMO
-    for (let i = 0; i < snakeBody.length; i++) {
+    for (const element of snakeBody) {
         
-        if(snakeHeadX == snakeBody[i][0] && snakeHeadY == snakeBody[i][1]){
+        if(snakeHeadX == element[0] && snakeHeadY == element[1]){
             gameOver = true;
-            alert("Game over!");
-            location.reload()
         }
         
     }
